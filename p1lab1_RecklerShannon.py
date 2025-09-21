abe8124922654f5a4f72177b882bd3478c2dbd01
smreckler1 <recklers6947@student.faytechcc.edu>
2025-09-20 20:48:57 -0400

    Add files via upload

---
+++ p1lab1_RecklerShannon.py
@@ -0,0 +1,8 @@
+#Title: Shannon Reckler
+#Date: 09192025
+#Assignment Name: P1 Lab 1
+#A brief description of the project: This assignment teaches the student how to create a Python program using Visual Studio Code. The goal is to get input from the user and display output.
+
+first_name = input('Enter your first Name:')
+last_name = input('Enter your last Name:')
+print ("Hello", first_name, last_name, "! Welcome to CTI-110")
